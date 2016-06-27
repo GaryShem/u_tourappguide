66c4026fbbf1efa92d4a8c55024c25b19283e076
Gary <Garyshemyakin@gmail.com>
2016-06-27 19:28:16 +0300

    automatic code formatting

--- app/src/main/java/com/udacity/garyshem/tourappguide/Attraction.java
+++ app/src/main/java/com/udacity/garyshem/tourappguide/Attraction.java
@@ -9,21 +9,24 @@ public class Attraction {
 
     /**
      * Constructor for entries with image
-     * @param name is the name of the object or event
-     * @param description is the text description of a place or event
+     *
+     * @param name            is the name of the object or event
+     * @param description     is the text description of a place or event
      * @param imageResourceId related image
      */
-    public Attraction(String name, String description, int imageResourceId){
+    public Attraction(String name, String description, int imageResourceId) {
         this.name = name;
         this.description = description;
         this.imageResourceId = imageResourceId;
     }
+
     /**
      * Constructor for entries without image
-     * @param name is the name of the object or event
+     *
+     * @param name        is the name of the object or event
      * @param description is the text description of a place or event
      */
-    public Attraction(String name, String description){
+    public Attraction(String name, String description) {
         this.name = name;
         this.description = description;
         imageResourceId = NO_RESOURCE_PROVIDED;
@@ -31,29 +34,31 @@ public class Attraction {
 
     /**
      * Method to determine whether an entry has an associated image resource
+     *
      * @return true if there is an associated image, false otherwise
      */
-    public boolean hasImage(){
+    public boolean hasImage() {
         return imageResourceId != NO_RESOURCE_PROVIDED;
     }
 
     /**
      * @return name of the event or location
      */
-    public String getName(){
+    public String getName() {
         return name;
     }
 
     /**
      * @return description of the event or location
      */
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
+
     /**
      * @return image resource ID
      */
-    public int getImageResourceId(){
+    public int getImageResourceId() {
         return imageResourceId;
     }
 }
